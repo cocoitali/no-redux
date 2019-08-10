6dc29a37affc7c45ef73e736ebe110ef172a1cd9
cocoitali <cmburatto@gmail.com>
2019-08-10 12:34:58 -0600

    Fleshed out reducer function in Store

--- src/Store.js
+++ src/Store.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export const Store = React.createContext();
 
 const initialState = {
-    episodes: [],
-    favourites: []
-  };
+  episodes: [],
+  favourites: []
+};
 
-function reducer() {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_DATA":
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+}
 
 export function StoreProvider(props) {
-    return <Store.Provider value='data from store'>{props.children}
-    </Store.Provider>
-  }
+  return (
+    <Store.Provider value="data from store">{props.children}</Store.Provider>
+  );
+}
